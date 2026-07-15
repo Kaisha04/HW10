a1271720cc0ff702e4a711c6d5d3efb9ae7252bc
ShunQ04 <113802850+Kaisha04@users.noreply.github.com>
2026-07-15 17:17:34 +0300

    final work

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -3,7 +3,7 @@
 Виведіть на екран значення елементів масиву, який повернув метод GetArray(), що розширює метод.*/
 
 using System;
-
+using System.Collections;
 
 namespace Task4;
 
@@ -11,7 +11,94 @@ class Program
 {
     static void Main()
     {
-
+        MyList<int> test = new MyList<int>();
     }
 }
 
+static class ExtensionClass
+{
+    public static T[] GetArray<T>(this MyList<T> list)
+    {
+        T[] array = new T[list.Length];
+        for (int i = 0; i < list.Length; i++)
+        {
+            array[i] = list[i];
+        }
+        return array;
+    }
+}
+
+class MyList<T>
+{
+    T[] array = new T[0];
+
+    public int Length
+    {
+        get
+        {
+            return array.Length;
+        }
+    }
+
+    public T this[int i]
+    {
+        get
+        {
+            if (i < 0 || i > array.Length - 1)
+            {
+                Console.WriteLine("Поза межами списку");
+            }
+            return array[i];
+        }
+        set
+        {
+            if (i < 0 || i > array.Length - 1)
+            {
+                Console.WriteLine("Поза межами списку");
+                return;
+            }
+
+            array[i] = value;
+        }
+    }
+
+    public MyList() { }
+    public MyList(T[] array)
+    {
+        this.array = array;
+    }
+
+    public void Add(T value)
+    {
+
+
+        T[] newArray = new T[array.Length + 1];
+        for (int i = 0; i < array.Length; i++)
+        {
+            newArray[i] = array[i];
+        }
+        newArray[newArray.Length - 1] = value;
+        array = newArray;
+
+    }
+
+    public void Delete(int index)
+    {
+        if (index >= 0 && index < array.Length)
+        {
+            T[] newArray = new T[array.Length - 1];
+            for (int i = 0, j = 0; i < array.Length; i++)
+            {
+                if (i == index) continue;
+                newArray[j] = array[i];
+                j++;
+
+            }
+            array = newArray;
+        }
+        else
+        {
+            Console.WriteLine("Немає такого елементу");
+        }
+    }
+}
