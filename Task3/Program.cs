5184f373310b18d424e2de8ec672b96bc8afbe64
ShunQ04 <113802850+Kaisha04@users.noreply.github.com>
2026-07-15 14:18:48 +0300

    finished task 3

---
+++ Task3/Program.cs
@@ -0,0 +1,63 @@
+﻿/*Створіть проект Console Application, де реалізуйте типізований клас "Чарівний мішок". Чарівність полягає в тому, що подарунок сам з'являється у мішку та залежить від того, хто намагається відкрити мішок.
+Причому подарунок для однієї істоти може з'явитись лише 1 раз на день. Використовуйте обмеження типу - інтерфейс із властивістю, що зберігає тип істоти, яка намагається отримати подарунок із мішка.*/
+
+using System;
+
+class Program
+{ 
+    static void Main()
+    {
+        MagicBag<ICreature> bag = new MagicBag<ICreature>();
+
+        Elf dobby = new Elf();
+        Human aragorn = new Human();
+
+        Console.WriteLine("--- Підходять вперше ---");
+        bag.GetPresent(dobby); 
+        bag.GetPresent(aragorn);
+
+        Console.WriteLine("\n--- Підходять вдруге за день ---");
+        bag.GetPresent(dobby); 
+    }
+}
+
+public class Elf : ICreature
+{
+    public string Present { get => "Мішок з грошима"; }
+
+
+}
+
+public class Ogr : ICreature
+{
+    public string Present { get => "Мішок з буловою"; }
+}
+public class Human : ICreature
+{
+    public string Present { get => "Мішок з їжою"; }
+}
+
+public interface ICreature
+{
+    public string Present { get; }
+}
+
+class MagicBag<T> where T : ICreature
+{
+    Dictionary<string, DateTime> time = new Dictionary<string, DateTime>();
+    public void GetPresent(T person)
+    {
+
+        if (!time.ContainsKey(person.GetType().Name))
+        {
+            Console.WriteLine(person.Present);
+            time.Add(person.GetType().Name, DateTime.Now);
+        }
+        else if (DateTime.Now.Date > time[person.GetType().Name].Date)
+        {
+            Console.WriteLine(person.Present);
+            time[person.GetType().Name] = DateTime.Now;
+        }else Console.WriteLine("День ще не минув ");
+
+    }
+}
---
+++ Task4/Program.cs
@@ -0,0 +1,17 @@
+﻿/*Використовуючи Visual Studio, створіть проект за шаблоном Console Application. 
+Створіть метод, що розширює: public static T[] GetArray(this MyList list) Застосуйте розширюючий метод до екземпляра типу MyList, розробленого в домашньому завданні 2 для даного уроку. 
+Виведіть на екран значення елементів масиву, який повернув метод GetArray(), що розширює метод.*/
+
+using System;
+
+
+namespace Task4;
+
+class Program
+{
+    static void Main()
+    {
+
+    }
+}
+
